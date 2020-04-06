fa17a54b8b93a7ea5677a1cc21e0661a2ea5a7cd
Mira Hiltunen <mira@exove.fi>
2020-04-06 15:09:57 +0300

    Change navigation styles when scrolling down

--- resources/assets/scripts/routes/common.js
+++ resources/assets/scripts/routes/common.js
@@ -1,6 +1,16 @@
 export default {
   init() {
-    // JavaScript to be fired on all pages
+    // Change menu when scrolling down
+    $(window).scroll(function () {
+      var $topbar = document.getElementById('topbar');
+      if ($(document).scrollTop() > 100) {
+        $topbar.classList.add('bg-white');
+        $topbar.classList.remove('py-4');
+      } else {
+        $topbar.classList.remove('bg-white');
+        $topbar.classList.add('py-4');
+      }
+    });
   },
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,8 +1,8 @@
-<header class="banner">
-  <div class="container flex flex-wrap justify-center h-full">
-    <a class="hover:border-transparent h-16" title="{{ get_bloginfo('name', 'display') }}" href="{{ home_url('/') }}">
-      @include('atoms/logo')
-    </a>
+<header id="topbar" class="banner fixed z-50 py-4 px-4 w-full flex flex-wrap justify-between items-center">
+  <a class="p-2 h-16" title="{{ get_bloginfo('name', 'display') }}" href="{{ home_url('/') }}">
+    @include('atoms/logo')
+  </a>
+  <div class="flex">
     @if (has_nav_menu('primary_navigation'))
       {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav nav--primary', 'container' => 'nav']) !!}
     @endif
