de6ed52f3d17b3e9eafe9e9b28c4a0fb94de4d9c
Mira Hiltunen <mira@exove.fi>
2020-04-11 11:40:25 +0300

    Added script to hide tobpar on scroll down

--- resources/assets/scripts/routes/common.js
+++ resources/assets/scripts/routes/common.js
@@ -1,7 +1,9 @@
 export default {
   init() {
-    // Change menu when scrolling down
+    var prevScrollpos = window.pageYOffset;
+
     $(window).scroll(function () {
+      // Add background color and shrink menu when scrolling down
       var $topbar = document.getElementById('topbar');
       if ($(document).scrollTop() > 100) {
         $topbar.classList.add('bg-white');
@@ -10,7 +12,19 @@ export default {
         $topbar.classList.remove('bg-white');
         $topbar.classList.add('py-4');
       }
+
+      // On mobile, hide topbar on scroll down and show on scroll up
+      var currentScrollPos = window.pageYOffset;
+      if (window.screen.width < 991) {
+        if (prevScrollpos > currentScrollPos) {
+          $topbar.style.top = '0';
+        } else {
+          $topbar.style.top = '-100px';
+        }
+        prevScrollpos = currentScrollPos;
+      }
     });
+
   },
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
