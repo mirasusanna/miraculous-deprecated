c5d17d857889694e33a76d329b712a9ad1b557c6
Mira Hiltunen <mira.hiltunen@outlook.com>
2020-10-26 22:15:42 +0200

    Improved header and footer layout, code style improvements

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,25 +1,29 @@
 <footer class="site-footer py-10 md:py-16 lg:py-20">
   <div class="container flex flex-wrap lg:flex-no-wrap">
     @if (has_nav_menu('footer_navigation'))
-      {!! wp_nav_menu(['theme_location' => 'footer_navigation', 'menu_class' => 'nav nav--footer', 'container' => 'nav']) !!}
+      {!! wp_nav_menu([
+        'theme_location' => 'footer_navigation',
+        'menu_class'     => 'nav nav--footer',
+        'container'      => 'nav'
+      ]) !!}
     @endif
     @if (is_active_sidebar('sidebar-footer-1'))
-      <div class="flex w-full lg:flex-1">
+      <div class="flex pl-4 w-full lg:flex-1">
         @php dynamic_sidebar('sidebar-footer-1') @endphp
       </div>
     @endif
     @if (is_active_sidebar('sidebar-footer-2'))
-      <div class="flex w-full lg:flex-1">
+      <div class="flex pl-4 w-full lg:flex-1">
         @php dynamic_sidebar('sidebar-footer-2') @endphp
       </div>
     @endif
     @if (is_active_sidebar('sidebar-footer-3'))
-      <div class="flex w-full lg:flex-1">
+      <div class="flex pl-4 w-full lg:flex-1">
         @php dynamic_sidebar('sidebar-footer-3') @endphp
       </div>
     @endif
     @if (is_active_sidebar('sidebar-footer-4'))
-      <div class="flex w-full lg:flex-1">
+      <div class="flex pl-4 w-full lg:flex-1">
         @php dynamic_sidebar('sidebar-footer-4') @endphp
       </div>
     @endif
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,11 +1,20 @@
-<header id="topbar" class="banner fixed top-0 z-50 py-4 px-4 w-full flex flex-wrap justify-between items-center">
-  <a class="p-2 h-16" title="{{ get_bloginfo('name', 'display') }}" href="{{ home_url('/') }}">
-    @include('components/logo')
-  </a>
-  <div class="flex">
-    <button class="inline-block lg:hidden">Menu</button>
+<header id="topbar" class="banner fixed top-0 z-50 py-4 px-4 w-full flex flex-no-wrap lg:flex-wrap justify-between bg-white">
+  <div class="flex lg:w-full justify-center">    
+    <a class="p-2 h-16" title="{{ get_bloginfo('name', 'display') }}" href="{{ home_url('/') }}">
+      @include('components/logo')
+    </a>
+  </div>
+  <div class="flex lg:w-full justify-center">
+    <button id="mobile-menu-toggle" class="inline-block lg:hidden">Menu</button>
     @if (has_nav_menu('primary_navigation'))
-      {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav nav--primary', 'container' => 'nav']) !!}
+      {!! wp_nav_menu([
+        'theme_location' => 'primary_navigation',
+        'menu_class' => 'nav nav--primary',
+        'container_id' => 'main-menu-container',
+        'menu_id' => 'main-menu',
+        'container' => 'nav',
+        'container_class' => 'main-menu-container hidden lg:flex'
+      ]) !!}
     @endif
   </div>
 </header>
